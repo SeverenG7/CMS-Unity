e363649cdeac8de866989429d6088527873f5616
@d.yuzefchik <d.yuzefchik@gmail.com>
2018-11-05 15:47:36 +0300

    example initial commit

---
+++ Assets/Rotor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour {
+
+    public GameObject GameObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, 0, -1);
+            GameObject.transform.Rotate(0, -1, 0);
+        }
+	}
+}
